f8ac4a5cb627f51926124753bcbccda873dd08be
Mohand Soluman <MSoluman@getgroup.com>
2024-01-11 14:31:32 +0200

    move description to redmfile

--- Design_Patterns/Program.cs
+++ Design_Patterns/Program.cs
@@ -14,28 +14,7 @@ public class Program
         // -------------------------------------------
         // 1-ComplexObject Construction:dd
         //------------------------------------------
-        /*
-         Creational Design Pattern: This means the pattern deals with object creation mechanisms,
-         trying to create objects in a manner suitable to the situation.
-         In simpler terms, it helps in creating objects effectively.
-        
-            -- normal-Way
-                Create computer without using the builder pattern
-                Involves creating an instance of the Computer class 
-                and setting properties individually.
-                Hard to remember the order and types of configurations.
-                Prone to mistakes, especially with optional configurations.
-
-         -- With Builder Pattern
-          Create computer  using the builder pattern
-
-           Provides a fluent and readable syntax for configuring the Computer object.
-           Each configuration method returns the builder instance, allowing for method chaining.
-           Optional configurations can be set more clearly and without relying on the order
-           of property assignments.
-
-        */
-
+      
         Computer myComputer1 = new Computer.Builder()
             .SetMakAddress("452-25")
             .SetCpu("Intel Core i7")
